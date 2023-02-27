16efa35773ecfeb99b4e66a943f0bb25f36a7334
Volkan Sagcan <volkanxvs@gmail.com>
2023-02-27 11:21:34 +0100

    Update dashboard

--- script.js
+++ script.js
@@ -1,178 +1,86 @@
 "use strict";
-function parseQueryString() {
-    let start = null;
-    let end = null;
-    if (location.search != "") {
-        const params = location.search.substr(1).split("&");
-        for (const param of params) {
-            const [name, value] = param.split("=", 2);
-            if (value === "") {
-                continue;
-            }
-            if (name == "start") {
-                start = new Date(value);
-            }
-            else if (name == "end") {
-                end = new Date(value);
-            }
-        }
+const colorPalette = [
+    '#7EB26D',
+    '#EAB839',
+    '#6ED0E0',
+    '#EF843C',
+    '#E24D42',
+    '#1F78C1',
+    '#BA43A9',
+    '#705DA0',
+    '#508642',
+    '#CCA300', // 9: dark sand
+];
+
+class Metrics {
+    constructor(name) {
+        this.label = name.split('/')[1];
+        this.commit = [];
+        this.timestamp = [];
+        this.value = [];
     }
-    return [start, end];
 }
-function unzip(entries, start, end) {
-    const revisionsMap = new Map();
-    const res = new Map();
+
+function generatePlot(title, timestamps, data) {
+    const options = {
+        title,
+        width: 700,
+        height: 325,
+        series: [{}]
+    };
+    data.forEach((it, idx) => {
+        options.series.push({
+            label: it.label.toString(),
+            stroke: colorPalette[idx],
+            width: 1.5,
+            // fill: "rgba(255,0,0,0.1)",
+        });
+    });
+    new uPlot(options, [timestamps, ...(data.map((it) => it.value.map((it) => it / 1000)))], document.getElementById("plots"));
+}
+
+function unzip(content) {
+    const entries = content.split('\n').filter((it) => it.length > 0).map((it) => JSON.parse(it));
+    const timestamps = entries.map((it) => it.timestamp);
+    const data = new Map();
     for (const entry of entries) {
-        if ((start != null && entry.timestamp < start) || (end != null && entry.timestamp > end)) {
-            continue;
-        }
-        for (let [key, [value, unit]] of Object.entries(entry.metrics)) {
-            if (!res.has(key)) {
-                res.set(key, {
-                    unit: unit,
-                    data: [],
-                    revision: [],
-                    timestamp: [],
-                });
+        for (let [key, value] of Object.entries(entry.metrics)) {
+            if (!data.has(key)) {
+                data.set(key, new Metrics(key));
             }
-            const r = res.get(key);
-            r.data.push(value);
-            r.timestamp.push(entry.timestamp);
-            const revisionHash = entry.revision.substr(0, 7);
-            r.revision.push(revisionHash);
-            revisionsMap.set(revisionHash, entry.timestamp);
+            const tmp = data.get(key);
+            tmp.commit.push(entry.commit);
+            tmp.timestamp.push(entry.timestamp);
+            tmp.value.push(Number(value));
         }
     }
-    const sortedRevisionsHash = Array.from(revisionsMap)
-        .sort(([, t1], [, t2]) => t1 - t2) // Sort by timestamp
-        .map(([hash]) => hash); // Extract only the hash
-    return [res, sortedRevisionsHash];
+    return [timestamps, data];
 }
-function show_notification(html_text) {
-    var notificationElem = document.getElementById('notification');
-    notificationElem.innerHTML = html_text;
-    notificationElem.classList.remove('hidden');
-    setTimeout(() => {
-        notificationElem.classList.add('hidden');
-    }, 3000);
-}
+
 async function main() {
-    const DATA_URL = "https://raw.githubusercontent.com/rust-analyzer/metrics/master/metrics.json";
-    const data = await (await fetch(DATA_URL)).text();
-    const entries = data.split('\n')
-        .filter((it) => it.length > 0)
-        .map((it) => JSON.parse(it));
-    const [start, end] = parseQueryString();
-    setTimeFrameInputs(start, end);
-    const [metrics, revisions] = unzip(entries, start ? +start / 1000 : null, end ? +end / 1000 : null);
-    const bodyElement = document.getElementById("inner");
-    const plots = new Map();
-    for (let [series, { unit, data, revision, timestamp }] of metrics) {
-        if (unit == "ms" && data.every(it => it >= 1000)) {
-            unit = "sec";
-            data = data.map(it => it / 1000);
-        }
-        else if (unit == "#instr") {
-            if (data.every(it => it > 1000)) {
-                unit = "# thousand instr";
-                data = data.map(it => it / 1000);
-                if (data.every(it => it > 1000)) {
-                    unit = "# million instr";
-                    data = data.map(it => it / 1000);
-                }
-            }
-        }
-        let plotName = series;
-        let seriesName;
-        const analysisStatsPrefix = 'analysis-stats/';
-        // Check for aggregated series of form "analysis-stats/<seriesName>/<plotName>"
-        //  - <seriesName> is the project (e.g. "ripgrep", "diesel")
-        //  - <plotName> is the metric (e.g. "total memory", "total time"), it cannot contain a `/`
-        if (plotName.startsWith(analysisStatsPrefix)) {
-            const plotNameStart = plotName.lastIndexOf("/");
-            const seriesNameStart = plotName.lastIndexOf("/", plotNameStart - 1);
-            seriesName = plotName.substring(seriesNameStart + 1, plotNameStart);
-            plotName = plotName.substring(plotNameStart + 1);
-        }
-        else {
-            seriesName = series;
-        }
-        let plot = plots.get(plotName);
-        if (!plot) {
-            plot = {
-                data: [],
-                layout: {
-                    title: plotName,
-                    xaxis: {
-                        type: 'date',
-                    },
-                    yaxis: {
-                        title: unit,
-                        rangemode: 'tozero'
-                    },
-                    width: Math.min(1200, window.innerWidth - 30),
-                    margin: {
-                        l: 50,
-                        r: 20,
-                        b: 100,
-                        t: 100,
-                        pad: 4,
-                    },
-                    legend: {
-                        orientation: window.innerWidth < 700 ? "h" : "v"
-                    }
-                }
-            };
-            plots.set(plotName, plot);
-        }
-        plot.data.push({
-            name: seriesName,
-            line: {
-                shape: "hv",
-            },
-            x: timestamp.map(n => new Date(n * 1000)),
-            y: data,
-            hovertext: revision,
-            hovertemplate: `%{y} ${unit}<br>(%{hovertext})`,
-        });
-    }
-    for (const [title, definition] of plots) {
-        const plotDiv = document.createElement("div");
-        definition.data.sort((a, b) => {
-            if (a.name < b.name) {
-                return -1;
-            }
-            else if (a.name > b.name) {
-                return 1;
-            }
-            else {
-                return 0;
-            }
-        });
-        Plotly.newPlot(plotDiv, definition.data, definition.layout);
-        plotDiv.on("plotly_click", (data) => {
-            const commit_hash = data.points[0].hovertext;
-            const url = `https://github.com/rust-analyzer/rust-analyzer/commit/${commit_hash}`;
-            const notification_text = `Commit <b>${commit_hash}</b> URL copied to clipboard`;
-            navigator.clipboard.writeText(url);
-            show_notification(notification_text);
-        });
-        bodyElement.appendChild(plotDiv);
-    }
-}
-function setDays(n) {
-    const timestamp = +new Date() - (n * 1000 * 60 * 60 * 24);
-    const date = new Date(timestamp);
-    setTimeFrameInputs(date, null);
-}
-function getTimeFrameInputs() {
-    const start = document.getElementsByName('start')[0];
-    const end = document.getElementsByName('end')[0];
-    return [start, end];
-}
-function setTimeFrameInputs(start, end) {
-    const [startInput, endInput] = getTimeFrameInputs();
-    startInput.value = start ? start.toISOString().split("T")[0] : "";
-    endInput.value = end ? end.toISOString().split("T")[0] : "";
+    const url = "https://raw.githubusercontent.com/PLC-lang/rusty/metrics/metrics.json";
+    const content = await (await fetch(url)).text();
+    let [timestamps, data] = unzip(content);
+    generatePlot("Build-Times oscat", timestamps, [
+        data.get("oscat/none"),
+        data.get("oscat/less"),
+        data.get("oscat/default"),
+        data.get("oscat/aggressive"),
+    ]);
+    generatePlot("Wall-Times rusty --check", timestamps, [
+        data.get("check/oscat"),
+    ]);
+    generatePlot("Wall-Times sieve-st", timestamps, [
+        data.get("sieve-st/none"),
+        data.get("sieve-st/less"),
+        data.get("sieve-st/default"),
+        data.get("sieve-st/aggressive"),
+    ]);
+    generatePlot("Wall-Times sieve-c", timestamps, [
+        data.get("sieve-c/0"),
+        data.get("sieve-c/1"),
+        data.get("sieve-c/2"),
+        data.get("sieve-c/3"),
+    ]);
 }
 main();
---
+++ script.ts
@@ -0,0 +1,123 @@
+const colorPalette = [
+    '#7EB26D', // 0: pale green
+    '#EAB839', // 1: mustard
+    '#6ED0E0', // 2: light blue
+    '#EF843C', // 3: orange
+    '#E24D42', // 4: red
+    '#1F78C1', // 5: ocean
+    '#BA43A9', // 6: purple
+    '#705DA0', // 7: violet
+    '#508642', // 8: dark green
+    '#CCA300', // 9: dark sand
+];
+
+class Metrics {
+    label: string;
+    commit: string[];
+    timestamp: number[];
+    value: number[];
+
+    constructor(name: string) {
+        this.label = name.split('/')[1];
+        this.commit = [];
+        this.timestamp = [];
+        this.value = [];
+    }
+}
+
+function generatePlot(title: string, timestamps: any[], data: Metrics[]) {
+    const options = {
+        title,
+        width: 700,
+        height: 325,
+        series: [{}]
+    };
+
+    data.forEach((it, idx) => {
+        options.series.push(
+            {
+                label: it.label.toString(),
+                stroke: colorPalette[idx],
+                width: 1.5,
+                // fill: "rgba(255,0,0,0.1)",
+            }
+        )
+    });
+
+    new uPlot(
+        options,
+        [timestamps, ...(data.map((it) => it.value.map((it) => it / 1000)))],
+        document.getElementById("plots")!
+    );
+}
+
+function unzip(content: string): [any[], Map<string, Metrics>] {
+    const entries = content.split('\n').filter((it) => it.length > 0).map((it) => JSON.parse(it));
+    const timestamps = entries.map((it) => it.timestamp);
+
+    const data = new Map<string, Metrics>();
+    for (const entry of entries) {
+        for (let [key, value] of Object.entries(entry.metrics)) {
+            if (!data.has(key)) {
+                data.set(key, new Metrics(key));
+            }
+
+            const tmp = data.get(key)!;
+            tmp.commit.push(entry.commit);
+            tmp.timestamp.push(entry.timestamp);
+            tmp.value.push(Number(value));
+        }
+    }
+
+    return [timestamps, data]
+}
+
+async function main() {
+    const url = "https://raw.githubusercontent.com/PLC-lang/rusty/metrics/metrics.json";
+    const content = await (await fetch(url)).text();
+    let [timestamps, data] = unzip(content);
+
+
+    generatePlot(
+        "Build-Times oscat",
+        timestamps,
+        [
+            data.get("oscat/none")!,
+            data.get("oscat/less")!,
+            data.get("oscat/default")!,
+            data.get("oscat/aggressive")!,
+        ]
+    );
+
+    generatePlot(
+        "Wall-Times rusty --check",
+        timestamps,
+        [
+            data.get("check/oscat")!,
+        ]
+    );
+
+    generatePlot(
+        "Wall-Times sieve-st",
+        timestamps,
+        [
+            data.get("sieve-st/none")!,
+            data.get("sieve-st/less")!,
+            data.get("sieve-st/default")!,
+            data.get("sieve-st/aggressive")!,
+        ]
+    );
+
+    generatePlot(
+        "Wall-Times sieve-c",
+        timestamps,
+        [
+            data.get("sieve-c/0")!,
+            data.get("sieve-c/1")!,
+            data.get("sieve-c/2")!,
+            data.get("sieve-c/3")!,
+        ]
+    );
+}
+
+main();
